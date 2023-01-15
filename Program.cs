c7011c384a73877e1ada13a4a86ab1e41d398e30
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-15 21:32:13 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,12 +10,16 @@
             //int n = str.Length;
             //Console.Write("\nAll possible strings are : ");
             //Iterative.IterativeMethod(str, 0, n - 1);
-            
-            String s = "ABC";
-            String answer = "";
 
-            Console.Write("\nAll possible strings are : ");
-            Recursive.RecursiveMethod(s, answer);
+            // String s = "ABC";
+            // String answer = "";
+
+            // Console.Write("\nAll possible strings are : ");
+            // Recursive.RecursiveMethod(s, answer);
+            PalindromAnagram obja = new PalindromAnagram();
+            obja.prime(2, 1000);
+            obja.checkpal();
+            obja.checkAnagram();
 
 
 
@@ -24,5 +28,11 @@
     }
 
 }
+
+
+
+
+
+
 
 
