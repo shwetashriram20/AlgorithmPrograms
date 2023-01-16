bafc260c616f3811f43557285548f02c9680a844
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-16 12:30:02 +0530

    Add files via upload

--- DataStructure/BalencedParanthesis/Program.cs
+++ DataStructure/BalencedParanthesis/Program.cs
@@ -1,1 +1,26 @@
+﻿namespace Stack
+{
+    using System;
+
+    /// <summary>
+    /// this is Program class for Stack operations.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("****************************Stack Operations*****************************");
+            Console.WriteLine("Simple Balanced Parentheses :");
+
+            BalanceParentheses check = new BalanceParentheses();
+            check.IsBalanceParentheses();
+
+        }
+                
+    }
+}
 
